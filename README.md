664ca6b31d8b3847ccc8fa1fe5a294fd963da902
Tome <tomecode@users.noreply.github.com>
2023-11-12 09:44:54 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # WpfDllInjectionExample
 Example of DLL Injection in WPF
+
+## Requirements
+VS Studio 2022
+dontet 6
+WPF
+
+## How it works
+The project has a unique DLL, ``${WpfDllInjectionExample}/d3d9.dll``, which includes specific code to display popup window demonstrating DLL injection (with 'DLL preloading attacks').
+When you start the application, this DLL is automatically copied to the app's home directory. As the app begins, the WPF Framework initalized then through PresentationFramework.dll, the special DLL is loaded automatically, triggering DLL injection (You will see empty popup window with button OK)
+
+To prevent this pre-load DLL injection, uncomment the lines between 28-32 in ``WpfDllInjectionExample/WpfDllInjectionExample/App.xaml.cs`` and start again the application.
+
+The main reason for the issue is default DLL loading logic (see: https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-setdefaultdlldirectories). By default, WPF (or/and the application) looks for DLLs in the home directory, which could be risky for DLL injection, also known as DLL pre-loading attack.
---
+++ WpfDllInjectionExample/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace WpfDllInjectionExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Value represent path: %windows%\system32 , for more details see: https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-setdefaultdlldirectories 
+        /// </summary>
+        private const int LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00000800;
+
+        /// <summary>
+        /// Sets list of paths that can be used to find native libraries such as drivers, etc.
+        /// </summary>
+        /// <param name="directoryFlags"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetDefaultDllDirectories(int directoryFlags);
+
+        public App()
+        {
+            //TODO: Uncomment the following code to disable injection
+            // Excludes the application's home directory from DLL searches
+            /*if (!SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS))
+            {
+                Console.WriteLine($"Failed to exclude application directory from DLL directories, error code: {Marshal.GetLastWin32Error()}");
+            }*/
+        }
+    }
+}
